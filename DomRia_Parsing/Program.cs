3913a4ac16e3dec9814a15ba45be67b2675bf5a3
Александр Горобчук <alexandrgorobchuk@gmail.com>
2021-10-13 23:54:26 +0300

    !

--- DomRia_Parsing/Program.cs
+++ DomRia_Parsing/Program.cs
@@ -14,12 +14,12 @@ namespace DomRia_Parsing
 {
     class Program
     {
-        static string url_parse = "https://dom.ria.com/node/searchEngine/v2/?links-under-filter=on&category=1&realty_type=2&operation_type=1&fullCategoryOperation=1_2_1&wo_dupl=1&page=0&state_id=12&city_id=12&limit=20&sort=inspected_sort&period=per_hour&ch=242_239,247_252,265_0,1437_1436:";
-        //static string url_parse = "https://dom.ria.com/node/searchEngine/v2/?links-under-filter=on&category=1&realty_type=2&operation_type=1&fullCategoryOperation=1_2_1&wo_dupl=1&page=0&state_id=12&city_id=12&limit=20&sort=inspected_sort&period=per_day&ch=242_239,247_252,265_0,1437_1436:";
-        static readonly IHtmlParser Parser = new HtmlParser();
-        static readonly List<string> AdsIdList = new List<string>() { "0" };
-        static readonly TelegramBotClient bot = new TelegramBotClient("1997294527:AAEkeioj8U3u7EaXkFftyxqcqnraVrcvVxs");
-        static readonly ChatId chatId = "-1001588392048";
+        static string urlForParse = "https://dom.ria.com/node/searchEngine/v2/?links-under-filter=on&category=1&realty_type=2&operation_type=1&fullCategoryOperation=1_2_1&wo_dupl=1&page=0&state_id=12&city_id=12&limit=20&sort=inspected_sort&period=per_hour&ch=242_239,247_252,265_0,1437_1436:";
+        //static string urlForParse = "https://dom.ria.com/node/searchEngine/v2/?links-under-filter=on&category=1&realty_type=2&operation_type=1&fullCategoryOperation=1_2_1&wo_dupl=1&page=0&state_id=12&city_id=12&limit=20&sort=inspected_sort&period=per_day&ch=242_239,247_252,265_0,1437_1436:";
+        static readonly IHtmlParser parser = new HtmlParser();
+        static readonly List<string> baceAdsIdList = new List<string>() { "0" };
+        static readonly TelegramBotClient telegramTockenBotClient = new TelegramBotClient("1997294527:AAEkeioj8U3u7EaXkFftyxqcqnraVrcvVxs");
+        static readonly ChatId telegramChatId = "-1001588392048";
         public static async Task Main(string[] args)
         {
             Console.WriteLine($"Start parsing {DateTime.Now}");
@@ -29,8 +29,8 @@ namespace DomRia_Parsing
                 {
                     using (var client = new HttpClient())
                     {
-                        IDocument html = await Parser.ParseDocumentAsync(await client.GetStringAsync(url_parse));
-                        JObject json = JObject.Parse(html.Source.Text); //Если Item = 0, то исключение
+                        IDocument html = await parser.ParseDocumentAsync(await client.GetStringAsync(urlForParse));
+                        JObject json = JObject.Parse(html.Source.Text);
                         if (json["count"].ToString().Contains("0"))
                         {
                             await Task.Delay(2000); 
@@ -38,12 +38,12 @@ namespace DomRia_Parsing
                         }
                         JArray jsonArray = (JArray)json["items"];
                         List<string> listId = jsonArray.Select(x => (string)x).ToList();
-                        IEnumerable<string> listIdNewAds = listId.Except(AdsIdList);
-                        foreach (string id in listIdNewAds)
+                        IEnumerable<string> listIdNewAds = listId.Except(baceAdsIdList);
+                        foreach (string item in listIdNewAds)
                         {
                             Console.WriteLine($"New Ads {DateTime.Now}");
-                            string urlAds = $"https://dom.ria.com/node/searchEngine/v2/view/realty/{id}?lang_id=4";
-                            IDocument parseforAds = await Parser.ParseDocumentAsync(await client.GetStringAsync(urlAds));
+                            string urlAds = $"https://dom.ria.com/node/searchEngine/v2/view/realty/{item}?lang_id=4";
+                            IDocument parseforAds = await parser.ParseDocumentAsync(await client.GetStringAsync(urlAds));
                             JObject jsonAds = JObject.Parse(parseforAds.Source.Text);
 
                             string location = $"ул. {jsonAds["street_name"]}, р‑н. {jsonAds["district_name"]}, г. {jsonAds["city_name"]}";
@@ -57,8 +57,8 @@ namespace DomRia_Parsing
                                 $"<b>Детально:</b> {apartment}\n" +
                                 $"<b>Дата публикции:</b> {publishing_date}\n";
 
-                            bot.SendTextMessageAsync(chatId, telegramMessage, ParseMode.Html).Wait();
-                            AdsIdList.Add(id);
+                            telegramTockenBotClient.SendTextMessageAsync(telegramChatId, telegramMessage, ParseMode.Html).Wait();
+                            baceAdsIdList.Add(item);
                         }
                         await Task.Delay(2000);
                     }
